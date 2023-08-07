d32042478bcfde6d8a15104b1413db8a8ef1830c
gudengxia <gudengxia@foxmail.com>
2023-08-07 20:39:33 +0800

    update0807

--- src/beavers.rs
+++ src/beavers.rs
@@ -1,8 +1,11 @@
 use idpf::*;
 use idpf::RingElm;
 use idpf::prg::*;
+use std::ops::Index;
+use std::error::Error;
 pub struct Beaver{
-    pub a: Vec<RingElm>
+    pub a: Vec<RingElm>,
+    pub n: usize
 }
 
 impl Beaver{
@@ -22,7 +25,7 @@ impl Beaver{
 
             let rd_bits = stream.next_bits(NUMERIC_LEN);
             rv[loc] = RingElm::from( bits_to_u32(&rd_bits[..NUMERIC_LEN]));
-            //rv[loc] = RingElm::from((i+1) as u32);
+            rv[loc] = RingElm::from((i+2) as u32);
         }// initialize the n atom-s which is in the 2^i-1 location
         //println!("{:?}", rv);
         // assign the other locations
@@ -36,21 +39,21 @@ impl Beaver{
                 j = j / 2;
             }
             //println!("bits={:?}", bits);
-            let mut r = RingElm::from(1u32);
+            let mut e = RingElm::from(1u32);
             let mut bit_loc = bits.len();
             while !bits.is_empty(){
                 if bits.pop().unwrap() == 1{
-                    r.mul(&rv[(1<<(bit_loc-1)) - 1]);
+                    e.mul(&rv[(1<<(bit_loc-1)) - 1]);
                 }
                 bit_loc -= 1;
             }
-            rv[i] = r;
+            rv[i] = e;
         } 
-        //println!("{:?}", rv);
-        Beaver { a: rv }
+        println!("{:?}", rv);
+        Beaver { a: rv, n: n}
     }
 
-    fn split(&self) -> (Beaver, Beaver){
+    pub fn split(&self) -> (Beaver, Beaver){
         let len = self.a.len();
         let mut rv1 = Vec::<RingElm>::new();
         let mut rv2 = Vec::<RingElm>::new();
@@ -66,8 +69,72 @@ impl Beaver{
             rv2.push(e);
         }
 
-        let bv1 = Beaver{a: rv1};
-        let bv2 = Beaver{a: rv2};
+        let bv1 = Beaver{a: rv1, n: len};
+        let bv2 = Beaver{a: rv2, n: len};
         (bv1, bv2)
     }
+
+    pub fn extendfrom(v: Vec<RingElm>)->Self{
+        let n = v.len();
+        let N: usize = (1<<n) - 1;
+        let mut rv = Vec::<RingElm>::new();
+        let zero = RingElm::from(0u32);
+        for _ in 0.. N{
+            rv.push(zero.clone());
+        }
+        for i in 0..=n-1{
+            let loc = (1<<i) - 1; // convert the loc to index by minus 1
+            rv[loc] = v[i].clone();
+            //rv[loc] = RingElm::from((i+1) as u32);
+        }// initialize the n atom-s which is in the 2^i-1 location
+
+        let mut e = rv[0].clone();
+        e.mul(&rv[1]);
+        rv[2] = e;
+
+        for i in 2..n{
+            let loc = (1<<i) - 1; //the i-th atom item.
+            let e = rv[loc].clone(); 
+        
+            for j in 1..=loc{
+                rv[loc+j] = rv[j-1].clone(); 
+                rv[loc+j].mul(&e);
+            }
+        }  
+        println!("{:?}", rv);
+        Beaver { a: rv, n: n }
+    }
+
+}
+
+impl Index<usize> for Beaver {
+    type Output = RingElm;
+
+    fn index(&self, index: usize) -> & RingElm {
+        let loc = 1<<index;
+        &self.a[loc-1]
+    }
+}
+
+//delta is extend from the opened ring values, and b is the beaver tuples shared by the two parites in offline phase
+pub fn Muls(delta: &Beaver, b: &Beaver, is_server: bool)->Result<RingElm, Box<dyn Error>>{
+    if delta.a.len() != b.a.len() && delta.n == b.n{
+        return Err("the two beaver tuples don't match".into());
+    }
+    
+    let n = delta.n;
+    let N = (1<<n) - 1; //0x0..01..1(n 1-s)
+    let mut r: RingElm = RingElm::from(0);
+    if is_server{
+        r.add(&delta.a[N-1]);
+    }
+
+    for i in 1..(N-1){
+        let mut unit = delta.a[i-1].clone();
+        let index = (!i) & N;
+        unit.mul(&b.a[index-1]);
+        r.add(&unit);
+    }
+
+    Ok(r)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,75 @@
 mod beavers;
 use beavers::*;
-
+use idpf::RingElm;
+use idpf::*;
 fn main() {
-    let beaver = Beaver::gen(4);
+    let n: usize = 2;
+    let mut v1= Vec::<RingElm>::new();
+    for i in 1..=n{
+        v1.push(RingElm::from(i as u32));
+    }
+
+    let mut v2= Vec::<RingElm>::new();
+    for i in 1..=n{
+        v2.push(RingElm::from((i+1) as u32));
+    }
+
+    let beaver = Beaver::gen(n);
+
+    let (b1, b2) = beaver.split();
+
+    let mut d1 = v1.clone();
+    let mut d2 = v2.clone();
+
+    for i in 0..n{
+        d1[i].sub(&b1[i]);
+        d2[i].sub(&b2[i]);
+    }
+
+    let mut d = d1.clone();
+    print!("d=(");
+    for i in 0..n{
+        d[i].add(&d2[i]);
+        print!("{:?},", d[i]);
+    }
+    println!(")");
+
+    let delta = Beaver::extendfrom(d);
+
+    let r1 = Muls(&delta, &b1, true).unwrap();
+    let r2 = Muls(&delta, &b2, false).unwrap();
+
+    let mut result = r1.clone();
+    result.add(&r2);
+    println!("{:?}", result); 
 }
 
 
 mod test{
     use crate::beavers::*;
+    use idpf::*;
+    use idpf::RingElm;
 
     #[test]
     fn beavers_gen_works(){
         let beaver = Beaver::gen(2);
+    }
+   
+    fn beaver_split_works()
+    {
+        let n = 4;
+        let b = Beaver::gen(n);
+        let (b1, b2) = b.split();
+
+        let mut r1 = b1.a.clone();
+        let r2 = b2.a.clone();
+
+        for i in 0..r1.len(){
+            r1[i].add(&r2[i]);
+        }
+
+        for i in 0..r1.len(){
+            assert_eq!(r1[i], b[i])
+        }
     }
 }
